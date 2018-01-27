6566d1ddf5a7345eac6d2d88115ec76f6c14b257
Jesse SMith <jesse.lee.smith@gmail.com>
2018-01-27 18:52:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ function nowServing(katzDeliLine){
 function currentLine(katzDeliLine)
 {
   if(katzDeliLine.length === 0){
-    return ""
-  }
-  var currentLine = "The line is currently: ";
-  for(var i = 0; i < katzDeliLine.length; i++){
-    currentLine += `${i + 1}. ${katzDeliLine[i]}`
+    return "The line is currently empty."
+  }else{
+    var currentLine = "The line is currently: ";
+    for(var i = 0; i < katzDeliLine.length; i++){
+      currentLine += `${i + 1}. ${katzDeliLine[i]}`
+    }
+    if(i < katzDeliLine.length - 1){
+      currentLine += ',';
+    }
   }
 }

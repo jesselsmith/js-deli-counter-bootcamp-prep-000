e5c568419b16a0d8d91b80622ed842541e142448
Jesse SMith <jesse.lee.smith@gmail.com>
2018-01-27 18:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ function takeANumber(katzDeliLine, newPerson){
   katzDeliLine.push(newPerson);
   return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`;
 }
-function nowServing(){
+function nowServing(katzDeliLine){
   if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
   }else{
     return katzDeliLine.shift();
+  }
+}
+
+function currentLine(katzDeliLine)
+{
+  var currentLine
+  for(var i = 0; i < katzDeliLine.length; i++){
+    
   }
 }
